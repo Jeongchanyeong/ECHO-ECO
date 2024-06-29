6cc3de3ad832107ea23e1d4e9392ed3c1ceb197c
정찬영 <q135794268020@gmail.com>
2024-06-30 03:24:55 +0900

    feat: 공통 Button 구현 완료

--- swype-client/src/common/Button.tsx
+++ swype-client/src/common/Button.tsx
@@ -4,38 +4,44 @@ import { DefaultTheme } from 'styled-components/dist/types';
 
 const CommonButton = styled.button<ButtonProps>`
   background-color: ${props => props.theme.colors.bgColor[props.bgColor]};
+  color: ${props => props.theme.colors.text[props.textColor]};
+
   width: ${props => props.width}; 
   height: ${props => props.height}; 
-  color: ${props => props.theme.colors.text[props.textColor]}; 
-  border-radius: 7px;
-  font-family: 'NanumSquareRound', sans-serif;
+  font-weight: ${props => props.theme.fontWeight.bold};
+
+  border-radius: 10px;
   font-size: 17px;
   text-align: center;
   letter-spacing: -3%;
-  transition: all 0.3s;
+  transition: all 0.2s;
   
 
   &:hover {
     opacity: 0.9;
   }
+
+  &:active {
+    transform: scale(0.97);
+  }
 `;
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  bgColor: keyof DefaultTheme['colors']['bgColor'];
+  children: React.ReactNode;
   width: string;
   height: string;
   textColor: keyof DefaultTheme['colors']['text'];
-  children: React.ReactNode;
+  bgColor: keyof DefaultTheme['colors']['bgColor'];
 }
 
 const Button = ({ bgColor, width, height, textColor, children, onClick }: ButtonProps) => {
   return (
     <CommonButton
-      onClick={onClick}
       bgColor={bgColor}
+      textColor={textColor}
       width={width}
       height={height}
-      textColor={textColor}
+      onClick={onClick}
     >
       {children}
     </CommonButton>
