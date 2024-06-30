8f568764279745fde55fe99b3460385e200117f3
정찬영 <q135794268020@gmail.com>
2024-06-30 18:43:03 +0900

    feat: 로그인 페이지 UI 구현 #19

---
+++ swype-client/src/pages/Login.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import { Container } from '../common/GlobalStyle';
+import LoginForm from '../components/LoginForm';
+import Login_Img from '../assets/Login_Img.png';
+
+const Wrapper = styled.div`
+background-color: #E1F3F4;
+
+width: 100%;
+height: 100vh;
+
+  background-image: url(${Login_Img}); // 배경 이미지 설정
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+`;
+
+const Login = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <LoginForm />
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Login;
