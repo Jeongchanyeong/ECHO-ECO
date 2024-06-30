72267641f2e02596327445f5614fa2dc80ef4865
정찬영 <q135794268020@gmail.com>
2024-06-30 14:19:36 +0900

    feat: 폰트 size rem값 추가  #3

--- swype-client/src/styled.d.ts
+++ swype-client/src/styled.d.ts
@@ -21,11 +21,25 @@ declare module 'styled-components' {
         blue: string;
       };
     };
-    fontWeight: {
-      light: number;
-      regular: number;
-      bold: number;
-      extraBold: number;
+    font: {
+      weight: {
+        light: 300;
+        regular: 400;
+        bold: 700;
+        extraBold: 800;
+      };
+      size: {
+        quizHeader1: '1.5rem'; // 24px
+        header1: '1.375rem'; // 22px
+        quizHeader2: '1.25rem'; // 20px
+        priceText2: '1.375rem'; // 22px
+        priceText: '1.0625rem'; // 17px
+        buttonText: '1.0625rem'; // 17px
+        body: '1rem'; // 16px
+        chipText: '0.9375rem'; // 15px
+        choose: '0.875rem'; // 14px
+        levelText: '0.8125rem'; // 13px
+      };
     };
   }
 }
--- swype-client/src/theme.ts
+++ swype-client/src/theme.ts
@@ -20,10 +20,24 @@ export const theme: DefaultTheme = {
       blue: '#23A1F8',
     },
   },
-  fontWeight: {
-    light: 300,
-    regular: 400,
-    bold: 700,
-    extraBold: 800,
+  font: {
+    weight: {
+      light: 300,
+      regular: 400,
+      bold: 700,
+      extraBold: 800,
+    },
+    size: {
+      quizHeader1: '1.5rem', // 24px
+      header1: '1.375rem', // 22px
+      quizHeader2: '1.25rem', // 20px
+      priceText2: '1.375rem', // 22px
+      priceText: '1.0625rem', // 17px
+      buttonText: '1.0625rem', // 17px
+      body: '1rem', // 16px
+      chipText: '0.9375rem', // 15px
+      choose: '0.875rem', // 14px
+      levelText: '0.8125rem', // 13px
+    },
   },
 };
