b87c9ee4175580ecdddeaf7a36b729d746bcd6d6
inseongei <twin2688@naver.com>
2024-07-08 23:49:02 +0900

    feat: 영상 페이지 기능  구현 #50

--- swype-client/src/pages/Movie.tsx
+++ swype-client/src/pages/Movie.tsx
@@ -3,8 +3,34 @@ import styled from 'styled-components';
 import Header from '../common/Header';
 import ReactPlayer from 'react-player';
 import Button from '../common/Button';
+import { useEffect, useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Movie() {
+  const navigate = useNavigate();
+  const [time, setTime] = useState<number>(30);
+  const [width, setWidth] = useState<number>(0);
+  const intervalIdRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    intervalIdRef.current = setInterval(() => {
+      setTime(prev => prev - 1);
+      setWidth(prev => prev + 3.4);
+    }, 1000);
+
+    return () => {
+      if (intervalIdRef.current) {
+        clearInterval(intervalIdRef.current);
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (time <= 0 && intervalIdRef.current) {
+      clearInterval(intervalIdRef.current);
+    }
+  }, [time]);
+
   return (
     <Container>
       <Header movie={true} />
@@ -16,29 +42,24 @@ export default function Movie() {
           height='100%' // 플레이어 크기 (세로)
           playing={true} // 자동 재생 on
           muted={true} // 자동 재생 on
-          controls={true} // 플레이어 컨트롤 노출 여부
-          light={false} // 플레이어 모드
-          pip={true} // pip 모드 설정 여부
-          poster={
-            'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg'
-          } // 플레이어 초기 포스터 사진
+          controls={false} // 플레이어 컨트롤 노출 여부
         />
         <TimeBox>
           <Timer>
-            <p>30</p>
+            <p>{time < 0 ? 0 : time}</p>
           </Timer>
-          <Progress></Progress>
+          <Progress>
+            <ProgressBar width={width}></ProgressBar>
+          </Progress>
         </TimeBox>
       </MovieBox>
-      <ButtonBox>
-        <Button
-          bgColor='blue'
-          textColor='white'
-          width='100%'
-          height='50px'
+      <ButtonBox time={time}>
+        <button
+          disabled={time !== 0}
+          onClick={() => navigate(`/`)}
         >
           퀴즈 기회 받기
-        </Button>
+        </button>
       </ButtonBox>
     </Container>
   );
@@ -72,15 +93,40 @@ const Timer = styled.div`
     justify-content: center;
     border: 2px solid #959595;
     border-radius: 20px;
+    padding: 5px;
   }
 `;
 
-const ButtonBox = styled.div`
+const ButtonBox = styled.div<{ time: number }>`
   padding:30px 15px;
   background-color: black;
+
+  button{
+    background-color: ${({ time }) => (time === 0 ? '#23A1F8' : '#D9D9D9')};
+    color: ${props => props.theme.colors.text.white};
+    font-weight: 700;
+    font-size: ${props => props.theme.font.size.buttonText};
+    width: 100%;
+    height: 50px;
+    border-radius: 10px;
+    text-align: center;
+    letter-spacing: -0.03rem;
+    transition: all 0.2s;
+  }
 `;
 
 const Progress = styled.div`
-  padding: 5px;
+  background-color: #fff;
+  height: 10px;
+  position: relative;
+
+`;
+
+const ProgressBar = styled.div<{ width: number }>`
+  max-width: 100%;
+  width: ${({ width }) => width}%;
+  height: 100%;
   background-color: #23A1F8;
+  position: absolute;
+  transition: all 1s;
 `;
