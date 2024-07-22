75925ac77e5276233599377e219b71a54bc70b52
정찬영 <q135794268020@gmail.com>
2024-07-22 15:59:16 +0900

    chore: API 함수 타입 지정 #67

--- swype-client/src/apis/trashImg.ts
+++ swype-client/src/apis/trashImg.ts
@@ -1,8 +1,13 @@
 import axios from 'axios';
 import { BASE_URL } from '../share/utils/OAuth';
 
+interface TrashImgData {
+  backgroundImage: string;
+  characterImage: string;
+}
+
 // 쓰레기 배경, 동물 받아오는 함수
-export const trashImg = async () => {
-  const res = await axios.get(`${BASE_URL}/character/trash`);
+export const getTrashImg = async (): Promise<TrashImgData> => {
+  const res = await axios.get<{ data: TrashImgData }>(`${BASE_URL}/character/trash`);
   return res.data.data;
 };
--- swype-client/src/apis/trashStatusCheck.ts
+++ swype-client/src/apis/trashStatusCheck.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { BASE_URL } from '../share/utils/OAuth';
 
 // 서버에서 isClean true false 받아오는 함수
-export const trashStatusCheck = async () => {
-  const res = await axios.get(`${BASE_URL}/trash/status`);
+export const trashStatusCheck = async (): Promise<boolean> => {
+  const res = await axios.get<{ data: { isClean: boolean } }>(`${BASE_URL}/trash/status`);
   return res.data.data.isClean;
 };
