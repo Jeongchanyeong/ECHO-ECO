73236d04ede190b66b7da91b267b09fa542b98f5
정찬영 <q135794268020@gmail.com>
2024-07-25 00:45:19 +0900

    chore: 스플레쉬 크기 수정, 비디오 시청 유무 코드 간소화

--- swype-client/src/apis/video/getVideoWatched.ts
+++ swype-client/src/apis/video/getVideoWatched.ts
@@ -15,13 +15,9 @@ export const getVideoWatched = (): Promise<{ isWatched: boolean }> => {
         },
       }
     )
-    .then(res => {
-      const isWatchedData = res.data.data;
-      console.log('isWatched data:', isWatchedData); // 서버에서 반환된 isWatched 데이터를 출력합니다.
-      return isWatchedData;
-    })
+    .then(res => res.data.data)
     .catch(error => {
-      console.error('error:', error.response.data); // 서버에서 반환된 에러 메시지를 출력합니다.
+      console.error('error:', error);
       throw error;
     });
 };
--- swype-client/src/pages/Splash.tsx
+++ swype-client/src/pages/Splash.tsx
@@ -27,9 +27,6 @@ const Splash = () => {
 const SplashGIF = styled.img`
   width: 100%;
   height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
 `;
 
 const fadeOut = keyframes`
